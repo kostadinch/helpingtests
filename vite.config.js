c4bd5285edae4ddd3b225e293352243c97427fb4
kostadinch <153559341+kostadinch@users.noreply.github.com>
2025-09-20 12:53:54 +0100

    Set up Vite + React Router for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/helpingtests/',
   server: {
     host: true,        // listen on 0.0.0.0
     port: 5173,        // optional: change if you like
